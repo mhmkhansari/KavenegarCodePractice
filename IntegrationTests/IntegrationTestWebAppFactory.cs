3a2bdcae1107a08238a5bbb18902a55824cb9950
Mohammad Hossein Khansari <Mohammad.Kh@behinrahkar.com>
2024-04-01 10:58:23 -0700

    Initial integration test for CI/CD setup

---
+++ IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,27 @@
+﻿using Kavehnegar.External.Presentation;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+
+namespace IntegrationTests
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .Build();
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _dbContainer.StopAsync();
+        }
+    }
+}
