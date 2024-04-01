dd9bdeaf8ea33a39d8c4bcd60b42b3a12d097403
Mohammad Hossein Khansari <Mohammad.Kh@behinrahkar.com>
2024-04-01 09:20:09 -0700

    Changes on gitignore

--- Kavehnegar.Core.Domain/obj/Debug/net8.0/Kavehnegar.Core.Domain.AssemblyInfo.cs
+++ Kavehnegar.Core.Domain/obj/Debug/net8.0/Kavehnegar.Core.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kavehnegar.Core.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ddd730927aff882076185a358612efd9489a3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab7656dd72e54f272be9530a0936c48da1ec33a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kavehnegar.Core.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kavehnegar.Core.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
